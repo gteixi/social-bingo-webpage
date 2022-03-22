a5e741d66dba9ebf0159e302f34049d90f862c0e
gteixi <gemmateixido25@gmail.com>
2022-03-22 18:26:44 +0100

    Create SocialMedia component

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,20 +1,21 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import SocialMedia from "../../components/SocialMedia/SocialMedia";
+
+import "./Home.scss";
 
 function Home() {
   return (
-    <>
-      <div>
-        <Link to="/about">QUI SOM</Link>
-        <Link to="/description">QUÈ ÉS UN BINGO MUSICAL</Link>
-        {/* <Link to="/description_more">INSTRUCCIONS</Link> */}
-        <Link to="/pictures">FOTOS</Link>
-        <Link to="/contact">CONTRACTANS</Link>
+    <div className="homeContainer">
+      <Link to="/about">QUI SOM</Link>
+      <Link to="/description">QUÈ ÉS UN BINGO MUSICAL</Link>
+      {/* <Link to="/description_more">INSTRUCCIONS</Link> */}
+      <Link to="/pictures">FOTOS</Link>
+      <Link to="/contact">CONTRACTANS</Link>
+      <div className="socialMedia">
+        <SocialMedia />
       </div>
-      <div>
-        <h1>This is the Home page</h1>
-      </div>
-    </>
+    </div>
   );
 }
 
