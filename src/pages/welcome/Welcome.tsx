2a31bc05dbfcd9e18a9450b11738d754d98f748d
gteixi <gemmateixido25@gmail.com>
2022-03-22 11:57:08 +0100

    Style of Welcome page done

--- src/pages/welcome/Welcome.tsx
+++ src/pages/welcome/Welcome.tsx
@@ -1,9 +1,28 @@
 import React from "react";
 
+import "./Welcome.css";
+
 function Welcome() {
   return (
-    <div>
-      <h1>This is the welcome page</h1>
+    <div className="mainContainer">
+      {/* UNMUTE COMPONENT */}
+      <div className="header">[ UNMUTE ]</div>
+      <div className="block">
+        <div className="block__text">
+          <h1>PREPARAT PER CANTAR BINGO?</h1>
+        </div>
+        <div className="block__answers">
+          <h1>SI</h1>
+          <h1>/</h1>
+          <h1>NO</h1>
+        </div>
+      </div>
+      <div className="socialMedia">
+        <p>redes</p>
+        <p>redes</p>
+        <p>redes</p>
+        <p>redes</p>
+      </div>
     </div>
   );
 }
