8be161a0bde84129210f396f5998cf6576c6dc37
gteixi <gemmateixido25@gmail.com>
2022-03-25 17:59:14 +0100

    Create Description page with styles

--- src/pages/description/Description.tsx
+++ src/pages/description/Description.tsx
@@ -1,10 +1,43 @@
 import React from "react";
+import Footer from "../../components/Footer/Footer";
+import MenuHeader from "../../components/MenuHeader/MenuHeader";
+import InfoTemplate from "../../components/InfoTemplate/InfoTemplate";
+
+import "./Description.scss";
 
 function Description() {
   return (
-    <div>
-      <h1>This is the Description page</h1>
-    </div>
+    <>
+      <MenuHeader />
+      <div className="descriptionContainer">
+        <InfoTemplate
+          title="QUÈ ÉS UN BINGO MUSICAL"
+          text="T’imagines ballar mentres guanyes premis?
+
+          Disposareu dels clàssics cartrons de Bingo pero aquest cop en canvi de números hi haurà els títols de les cançons que haureu d’anar esbrinant a mesura que les aneu sentint, perquè sí, la música serà en directe."
+        />
+        <p className="descriptionContainer__title">INSTRUCCIONS</p>
+        <div className="descriptionContainer__steps">
+          <p>STEP 1: COMPRA EL TEU CARTRÓ A LA BARRA.</p>
+          <p>
+            STEP 2:ESTIGUES PUNTUAL DAVANT L’ESCENARI PER COMENÇAR A SENTIR LES
+            CANÇONS DEL REPERTORI.
+          </p>
+          <p>
+            STEP 3: IDENTIFICA EL TÍTOL DE LA CANÇÓ DEL REPERTORI QUE ANEM
+            TOCANT.
+          </p>
+
+          <p>STEP 4: BÚSCA EL TÍTOL DE LA CANÇÓ AL TEU CARTRÓ I TATXA’L!</p>
+
+          <p>
+            STEP 5: ESTIGUES ATENT DE FER LÍNEA O BINGO I SI ES AIXÍ CRIDA I
+            APROPA’T AL ESCENARI PER FER LA RESPECTIVA COMPROVACIÓ.
+          </p>
+        </div>
+        <Footer />
+      </div>
+    </>
   );
 }
 
