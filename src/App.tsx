cb41270312cd7f2fb80cd991e31b9fc54339b327
gteixi <gemmateixido25@gmail.com>
2022-04-20 15:08:47 +0200

    Creation of new page for NoOption answer redirection

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,7 @@ import Description from "./pages/description/Description";
 import Home from "./pages/home/Home";
 import Pictures from "./pages/pictures/Pictures";
 import Welcome from "./pages/welcome/Welcome";
+import NoOption from "./pages/welcome/noOption/NoOption";
 
 import "./App.scss";
 
@@ -14,6 +15,7 @@ function App() {
   return (
     <Routes>
       <Route path="/" element={<Welcome />} />
+      <Route path="/com-que-no" element={<NoOption />} />
       <Route path="/home" element={<Home />} />
       <Route path="/about" element={<About />} />
       <Route path="/description" element={<Description />} />
