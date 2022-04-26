53b2590e943c90dc04156163f397a8eb12890dd8
gteixi <gemmateixido25@gmail.com>
2022-04-26 16:07:36 +0200

    Change phone number footer data

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -7,7 +7,7 @@ function Footer() {
     <div className="footerContainer">
       <div className="footerContainer__logoInfo">
         <img src="./../../assets/whatsapp.png" alt="Whatsapp" />
-        <p>654773382</p>
+        <p>620868556</p>
       </div>
       <div className="footerContainer__logoInfo">
         <img src="./../../assets/mail.png" alt="Mail" />
