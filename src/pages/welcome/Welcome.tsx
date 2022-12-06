7014d1e1718ddbfe30a4484f5e63178c171558ac
gteixi <gemmateixido25@gmail.com>
2022-12-06 18:01:59 +0100

    update website 2.0

--- src/pages/welcome/Welcome.tsx
+++ src/pages/welcome/Welcome.tsx
@@ -4,26 +4,46 @@ import SocialMedia from "../../components/SocialMedia/SocialMedia";
 import "./Welcome.scss";
 
 function Welcome() {
+  // return (
+  //   <div className="welcomeContainer">
+  //     {/* UNMUTE COMPONENT */}
+  //     <div className="welcomeContainer__header"> {/* UNMUTE COMPONENT */}</div>
+  //     <div className="welcomeContainer__block">
+  //       <div className="welcomeContainer__block__text">
+  //         <h1>READY PER CANTAR BINGO?</h1>
+  //       </div>
+  //       <div className="welcomeContainer__block__answers">
+  //         <a href="/home" className="welcomeContainer__block__answers--yes">
+  //           SI
+  //         </a>
+  //         <p className="welcomeContainer__block__answers--or">/</p>
+  //         <a
+  //           href="/com-que-no?"
+  //           className="welcomeContainer__block__answers--no"
+  //         >
+  //           NO
+  //         </a>
+  //       </div>
+  //     </div>
+  //     <div className="welcomeContainer__socialMedia">
+  //       <SocialMedia />
+  //     </div>
+  //   </div>
+  // );
+
   return (
     <div className="welcomeContainer">
-      {/* UNMUTE COMPONENT */}
-      <div className="welcomeContainer__header"> {/* UNMUTE COMPONENT */}</div>
+      <div className="welcomeContainer__header">
+        <img
+          src="./../../assets/lettering.png"
+          className="welcomeContainer__header__img"
+          alt="Logo"
+        />
+      </div>
       <div className="welcomeContainer__block">
-        <div className="welcomeContainer__block__text">
-          <h1>READY PER CANTAR BINGO?</h1>
-        </div>
-        <div className="welcomeContainer__block__answers">
-          <a href="/home" className="welcomeContainer__block__answers--yes">
-            SI
-          </a>
-          <p className="welcomeContainer__block__answers--or">/</p>
-          <a
-            href="/com-que-no?"
-            className="welcomeContainer__block__answers--no"
-          >
-            NO
-          </a>
-        </div>
+        <a href="/about" className="welcomeContainer__img">
+          <img src="./../../assets/main-logo.png" alt="Logo" />
+        </a>
       </div>
       <div className="welcomeContainer__socialMedia">
         <SocialMedia />
