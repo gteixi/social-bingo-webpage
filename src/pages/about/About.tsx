55e3dfd4266cb9ff8efb7b779b99b88d03a4177c
gteixi <gemmateixido25@gmail.com>
2022-12-06 18:42:39 +0100

    update text description

--- src/pages/about/About.tsx
+++ src/pages/about/About.tsx
@@ -16,9 +16,7 @@ function About() {
           text={[
             `Som La Social, un grup de funk i disco del Poblenou (Barcelona) amb més de 4 anys d’experiència tocant als escenaris.`,
             `
-            Ens caracteritzem per la nostre versatilitat tocant temes dels anys 70 i 80 fins als hits de l’actualitat.`,
-            `
-            És per això que després de 4 anys hem volgut crear un nou projecte on poder barrejar tot tipus de cançons presentant el BINGO MUSICAL ple de sorpreses i ganes de diversió.`,
+            Ens caracteritzem per la nostre versatilitat tocant temes dels anys 70 i 80 fins als hits de l’actualitat!`,
           ]}
         />
         <img
